cdb84bb503eb298729beab4586dfa69ef20be58b
BKY\bucky <buckyhelmsmash@gmail.com>
2022-05-03 12:12:41 +0700

    add: Nav component

--- components/Nav.js
+++ components/Nav.js
@@ -5,13 +5,14 @@ function Nav() {
     const router = useRouter();
     return (
         <nav className='relative'>
-            <div className='absolute top-0 left-0 bg-gradient-to-r from-[#06202A] h-10 w-1/12'/>
+            {/*<div className='absolute top-0 left-0 bg-gradient-to-r from-[#06202A] h-10 w-1/12'/>*/}
+            <div className='absolute top-0 left-0 bg-gradient-to-r from-[#06202A] h-10 w-1/12 z-50'/>
             <div className='flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide'>
                 {Object.entries(requests).map(([key, {title, url}]) => (
                     <h2
                         key={key}
                         onClick={() => router.push(`/?genre=${key}`)}
-                        className='cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500 last:pr-24'
+                        className='cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500 first:pl-20 last:pr-24'
                     >
                         {title}
                     </h2>
